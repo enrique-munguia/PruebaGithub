677ece27e03c274e8bb048719ebbce94620c2463
enrique.munguia <enrique.munguia@globant.com>
2016-02-27 12:36:02 -0600

    Añade metodo
    Este es un método de prueba

--- src/Main.java
+++ src/Main.java
@@ -3,4 +3,8 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 	}
+	
+	public static void metodo() {
+		System.out.println("Soy un método");
+	}
 }
